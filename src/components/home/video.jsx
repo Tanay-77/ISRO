f7c0a85c6e38748a2919ed6a75dfc18d750f2073
Tanay-77 <tanaymahajan7@gmail.com>
2025-10-05 20:19:57 +0530

    home section created

---
+++ src/components/home/video.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const video = () => {
+  return (
+    <div className='h-full w-full'>
+      <video  className='h-full w-full' autoPlay muted loop src="https://www.nasa.gov/wp-content/uploads/2025/09/a2-flyby-1min-2160p30orig.mp4"></video>
+    </div>
+  )
+}
+
+export default video
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
-
+import Video from '../components/home/video'
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <Video />
+    </div>
   )
 }
 
