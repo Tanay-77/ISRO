5be557814b627b6fa8455699df0633259f451878
Tanay-77 <tanaymahajan7@gmail.com>
2025-10-07 21:54:46 +0530

    landing page completed

--- src/components/home/homeBottomText.jsx
+++ src/components/home/homeBottomText.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 const HomeBottomText = () => {
   return (
-    <div>homeBottomText</div>
+    <div className='text-white font-[font2] flex items-center justify-center gap-3'>
+  <Link to='/projects' className='text-[6vw]  leading-[6vw] uppercase border-4 border-solid border-white rounded-full px-10 pt-1 pb-0 hover:border-blue-400 hover:text-blue-400 '>Projects</Link>
+  <Link to='/history' className='text-[6vw] leading-[6vw] uppercase border-4 border-solid border-white rounded-full px-10 pt-1 pb-0  '>History</Link>
+    </div>
   )
 }
 
--- src/pages/History.jsx
+++ src/pages/History.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const History = () => {
   return (
-    <div>History</div>
+    <div className='font-[font2] '>
+      <div className='mt-[55vh] '>
+        <h1 className='text-[20vw] text-center uppercase leading-[18vw]'>hflaljskd</h1>
+      </div>
+    </div>
   )
 }
 
