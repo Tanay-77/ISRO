838c98b2238dc69d4acb8d5c223cfbcd33e078ab
Tanay-77 <tanaymahajan7@gmail.com>
2025-10-08 21:44:46 +0530

    history page started

--- src/pages/History.jsx
+++ src/pages/History.jsx
@@ -2,10 +2,15 @@ import React from 'react'
 
 const History = () => {
   return (
-    <div className='font-[font2] '>
+    <div>
+      <div className='font-[font2] text-white'>
       <div className='mt-[55vh] '>
-        <h1 className='text-[20vw] text-center uppercase leading-[18vw]'>hflaljskd</h1>
+          <h1 className='text-[20vw] text-center uppercase leading-[18vw]'>History of ISRO</h1>
       </div>
+      <div className='pl-[40%] mt-20'>
+        <p className='text-6xl '>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Indian Space Research Organisation (ISRO) is the space agency of India. The organisation is involved in science, engineering and technology to harvest the benefits of outer space for India and the mankind. </p>
+      </div>
+    </div>
     </div>
   )
 }
