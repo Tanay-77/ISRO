0749687d207585a6eb449349adc57d280c8d2f0a
Tanay-77 <tanaymahajan7@gmail.com>
2025-10-08 22:10:45 +0530

    history page started

--- src/pages/History.jsx
+++ src/pages/History.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 const History = () => {
   return (
-    <div>
-      <div className='font-[font2] text-white'>
+   <div>
+     <div className='section1'>
+      <div className='absolute overflow-hidden h-[20vw] w-[15vw] rounded-3xl top-48 left-[30vw] bg-red-500 '>
+            <img className='h-full w-full object-cover' src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6e/A._P._J._Abdul_Kalam.jpg/1015px-A._P._J._Abdul_Kalam.jpg" alt="" />
+      </div>
+      <div className='relative font-[font2] text-white'>
       <div className='mt-[55vh] '>
           <h1 className='text-[20vw] text-center uppercase leading-[18vw]'>History of ISRO</h1>
       </div>
@@ -12,6 +16,9 @@ const History = () => {
       </div>
     </div>
     </div>
+    <div className="section2 h-screen"></div>
+   </div>
+    
   )
 }
 
