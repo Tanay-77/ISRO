4970d6755f9d29447725ec7c0d21f08c266b3a70
Tanay-77 <tanaymahajan7@gmail.com>
2025-10-09 22:42:21 +0530

    history page updated

--- src/pages/History.jsx
+++ src/pages/History.jsx
@@ -1,11 +1,41 @@
-import React from 'react'
-
+import React, { useRef } from 'react'
+import {useGSAP} from '@gsap/react'
+import { ScrollTrigger } from 'gsap/all'
+import gsap from 'gsap'
 const History = () => {
+  const imageDevRef = useRef(null)
+  const imageRef =useRef(null)
+  const imageArray = ['https://www.isro.gov.in/media_isro/image/chairmanimages/resized/Sarabhai_medium.jpg.webp',
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6e/A._P._J._Abdul_Kalam.jpg/1015px-A._P._J._Abdul_Kalam.jpg',
+    'https://www.isro.gov.in/media_isro/image/chairmanimages/S_Dhawan.jpg.webp',
+    'https://www.isro.gov.in/media_isro/image/chairmanimages/MGK_Menon.jpg.webp',
+    'https://www.isro.gov.in/media_isro/image/chairmanimages/U_R_Rao.jpg.webp',
+    'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRXA_MWTeChdHisZmwq6ninGsU2unsO5J0U3z_EdGD3agBvUiVAbvQDsl8fs7ShAkLA8YMf3mvWwI7HKnGdYrZNghgZtt8Pypobk9n9b_OlJw',
+    'https://www.isro.gov.in/media_isro/image/chairmanimages/madhavannair.jpg.webp',
+    'https://www.isro.gov.in/media_isro/image/chairmanimages/drksivan1.jpg.webp',
+    'https://www.isro.gov.in/media_isro/image/chairman.png.webp'
+
+  ]
+  gsap.registerPlugin(ScrollTrigger)
+  
+  useGSAP(function(){
+    gsap.to(imageDevRef.current,{
+      scrollTrigger:{
+        trigger:imageDevRef.current,
+        markers:true,
+        start:'top 28.1%',
+        end:'top -100%',
+        pin:true,
+        scrub:true
+      }
+    })
+  })
+  
   return (
    <div>
      <div className='section1'>
-      <div className='absolute overflow-hidden h-[20vw] w-[15vw] rounded-3xl top-48 left-[30vw] bg-red-500 '>
-            <img className='h-full w-full object-cover' src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6e/A._P._J._Abdul_Kalam.jpg/1015px-A._P._J._Abdul_Kalam.jpg" alt="" />
+      <div ref={imageDevRef} className='absolute overflow-hidden h-[20vw] w-[15vw] rounded-3xl top-44 left-[30vw] bg-red-500 '>
+            <img ref={imageRef} className='h-full w-full object-cover' src="https://www.isro.gov.in/media_isro/image/chairmanimages/resized/Sarabhai_medium.jpg.webp" alt="" />
       </div>
       <div className='relative font-[font2] text-white'>
       <div className='mt-[55vh] '>
