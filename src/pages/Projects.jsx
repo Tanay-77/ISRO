4b8514a469c67cae64a819b055e987472a79acce
Tanay-77 <tanaymahajan7@gmail.com>
2025-11-05 22:48:05 +0530

    added project page

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 const Projects = () => {
   return (
-    <div>Projects</div>
+    <div className='text-white p-4'>
+      <div className='pt-[30vh]'>
+        <h2 className='font-[font2] text-[9vw] uppercase '>Projects</h2>
+      </div>
+      <div>
+        <div className='w-full h-[700px] -mt-10 flex gap-4 mb-4 bg-white'>
+          <div className='w-1/2 h-full bg-yellow-300 '></div>
+          <div className='w-1/2 h-full  bg-blue-400'></div>
+        </div>
+        
+       
+      </div>
+    </div>
   )
 }
 
