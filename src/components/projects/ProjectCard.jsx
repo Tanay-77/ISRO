47ede696ba3a2afc973c7cd1f3851a78f2043446
Tanay-77 <tanaymahajan7@gmail.com>
2025-11-06 22:40:55 +0530

    added project page

--- src/components/projects/ProjectCard.jsx
+++ src/components/projects/ProjectCard.jsx
@@ -6,13 +6,13 @@ const ProjectCard = (props) => {
           <div className='w-1/2 h-full group relative transition-all rounded-none hover:rounded-[50px] overflow-hidden'>
           <img className='h-full w-full object-cover' src={props.image1} alt="" />
           <div className='opacity-0 group-hover:opacity-100 absolute top-0 flex items-center justify-center left-0 h-full w-full bg-black/10'>
-            <h2 className='font-[font1] text-4xl text-black border-black border-2 pt-2 px-4 rounded-full '>LVM3-M5/CMS-03 Mission</h2>
+            <h2 className='font-[font1] text-4xl text-white border-white border-2 pt-2 px-4 rounded-full '>{props.text1}</h2>
           </div>
           </div>
           <div className='w-1/2 h-full group relative transition-all rounded-none hover:rounded-[50px] overflow-hidden'>
           <img className='h-full w-full object-cover' src={props.image2} alt="" />
           <div className='opacity-0 group-hover:opacity-100 absolute top-0 flex items-center justify-center left-0 h-full w-full bg-black/10'>
-            <h2 className='font-[font1] text-4xl text-black border-black border-2 pt-2 px-4 rounded-full '>LVM3-M5/CMS-03 Mission</h2>
+            <h2 className='font-[font1] text-4xl text-white border-white border-2 pt-2 px-4 rounded-full '>{props.text2}</h2>
           </div>
           </div>
         </div>
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -4,13 +4,19 @@ import ProjectCard from '../components/projects/ProjectCard'
 const Projects = () => {
   const projects =[{
     image1:'https://www.isro.gov.in/media_isro/image/index/Recent/lvm3m5_rec03.png.webp',
-    image2:'https://www.isro.gov.in/media_isro/image/index/Recent/gslvf16_rec.png.webp'},
+    text1:'LVM3-M5/CMS-03 Mission',
+    image2:'https://www.isro.gov.in/media_isro/image/index/Recent/gslvf16_rec.png.webp',
+    text2:'GSLV-F16/NISAR Mission'},
     {
     image1:'https://www.isro.gov.in/media_isro/image/index/Recent/pslvc61_recent.jpg',
-    image2:'https://www.isro.gov.in/media_isro/image/index/Recent/gslvf15_rec.png'},
+    text1:'PSLV-C61 / EOS-09 Mission',
+    image2:'https://www.isro.gov.in/media_isro/image/index/Recent/gslvf15_rec.png',
+    text2:'GSLV-F15 / NVS-02 MISSION'},
     {
       image1:'https://cdn.britannica.com/92/126092-050-7A610AAE/Artist-conception-lunar-probe-Chandrayaan-1.jpg',
-      image2:'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/Mars_Orbiter_Mission_Over_Mars_%2815237158879%29.jpg/330px-Mars_Orbiter_Mission_Over_Mars_%2815237158879%29.jpg'
+      text1:'Chandrayaan',
+      image2:'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/Mars_Orbiter_Mission_Over_Mars_%2815237158879%29.jpg/330px-Mars_Orbiter_Mission_Over_Mars_%2815237158879%29.jpg',
+      text2:'Mars Orbiter Mission'
     
 
   }]
@@ -23,7 +29,7 @@ const Projects = () => {
       </div>
       <div className='-mt-10'>
          {projects.map((elem, index)=>{
-          return <ProjectCard key={index} image1={elem.image1}  image2={elem.image2} />
+          return <ProjectCard key={index} image1={elem.image1}  image2={elem.image2} text1={elem.text1} text2={elem.text2} />
 
          })}
       </div>
