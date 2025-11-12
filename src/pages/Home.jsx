8a1fcc65c721810b478c359bf1e328e2addd31b7
Tanay-77 <tanaymahajan7@gmail.com>
2025-11-12 22:50:49 +0530

    Fix case-sensitivity issue with Video component import for Linux/Vercel deployment

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react'
-import Video from '../components/home/Video'
+import Video from '../components/home/video'
 import HomeHeroText from '../components/home/HomeHeroText'
 import HomeBottomText from '../components/home/HomeBottomText'
 import './Home.css'
